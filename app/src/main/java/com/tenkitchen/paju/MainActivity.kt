e4318d44ab554627cf8a7518bd4d7c086255615b
leesh <yejifather@gmail.com>
2022-07-15 16:53:21 +0900

    2022.07.15 퇴근 전

--- app/src/main/java/com/tenkitchen/paju/MainActivity.kt
+++ app/src/main/java/com/tenkitchen/paju/MainActivity.kt
@@ -8,4 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    // 서버 통신은 HttpUrlConnection, Volley, OkHttp, [Retrofit2]
+    // https://velog.io/@jini0318/Android-Retrofit2%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%9C-API-%EC%84%9C%EB%B2%84%ED%86%B5%EC%8B%A0
 }
